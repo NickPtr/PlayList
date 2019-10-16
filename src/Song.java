e571c2288992c09b3fd6ed32545cdfdd5ce16860
NickPtr <44138348+NickPtr@users.noreply.github.com>
2019-10-16 15:20:53 +0300

    final

--- src/Song.java
+++ src/Song.java
@@ -7,9 +7,15 @@ public class Song
      ArrayList<String> songlist = new ArrayList<>();
      public Song()
      {
-         songlist.add("Ena\t\t"+"Mpampis\t\t"+"210\t\t"+"Tipota");
-         songlist.add("Duo\t\t"+"Mpampis\t\t"+"200\t\t"+"Tipota2");
-         songlist.add("Duo\t\t"+"Mpampis\t\t"+"200\t\t"+"Tipota2");
-         songlist.add("Duo\t\t"+"Lola\t\t"+"200\t\t"+"Tipota2");
+         songlist.add("NY\t\t"+"Sinatra\t\t"+"197\t\t"+"Duets");
+         songlist.add("Imagine\t\t"+"Lenon\t\t"+"195\t\t"+"Imagine");
+         songlist.add("LOVE\t\t"+"Sinatra\t\t"+"152\t\t"+"Grates Love Songs");
+         songlist.add("Help\t\t"+"Beatles\t\t"+"140\t\t"+"Help!");
+         songlist.add("Hello\t\t"+"Adele\t\t"+"366\t\t"+"25");
+         songlist.add("Remedy\t\t"+"Adele\t\t"+"245\t\t"+"25");
+         songlist.add("Work\t\t"+"Rihana\t\t"+"199\t\t"+"Anti");
+         songlist.add("Thriler\t\t"+"Jackson\t\t"+"424\t\t"+"Thriller 25");
+         songlist.add("Granade\t\t"+"Bruno\t\t"+"236\t\t"+"Doo-Wops");
+         songlist.add("Happy\t\t"+"Farel\t\t"+"244\t\t"+"Happy");
      }
 }
